70ff3cb91ee94f2b6798b3109132e13d0f48e13c
hemap20 <105234527+hemap20@users.noreply.github.com>
2024-06-24 11:09:40 +0000

    pos_to_cont

--- POSCAR_to_CONTCAR.C
+++ POSCAR_to_CONTCAR.C
@@ -2,18 +2,19 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <vector>
 #include <string>
 
 int main(){
     
     std::string line;
     int n_atom_types=0;
-    double box_dim[3] = {0}; //Lx, Ly, Lz
+    double box_dim[3][3] = {0}; //Lx, Ly, Lz
 
     
     std::ifstream input("POSCAR");
 
-    getline(input, line);   //KCl
+    std::getline(input, line);   //KCl
     std::istringstream atoms(line);
 
     std::string token; //tokens of line stored here
@@ -28,35 +29,78 @@ int main(){
 
 
     //1.0
-    getline(input, line);   
+    std::getline(input, line);   
+    std::istringstream val(line);
+    double value = 0;
+    val >> value;
+
 
     //box dimensions
     for(int i=0; i<3; i++){
-        int box_token = 0;
-        getline(input, line);
+        std::getline(input, line);
         std::istringstream box(line);
-        if(box >> box_token){
-            box_dim[i] = box_token;
-        }
+        box >> box_dim[i][0] >> box_dim[i][1] >> box_dim[i][2];
     }
 
     //number of atoms of each type
-    getline(input, line);
+    std::getline(input, line);
     std::istringstream atom_type(line);
-    int n_atoms_per_type[n_atom_types] = {0};
+    int n_atoms_per_type[n_atom_types];
+    int total_n_atoms = 0;
     for(int i=0; i<n_atom_types; i++){
-        
-        
+        int token_value;
+        atom_type >> token_value;
+        n_atoms_per_type[i] = token_value;
+        total_n_atoms += token_value;
     }
 
-     
-    
-    
+    //coordinate system   
+    std::string coordinate_sys;
+    std::getline(input, coordinate_sys);
 
+    //coordinates of the particles
+    double positions[total_n_atoms][3];
+    for(int i=0; i<total_n_atoms; i++){
+        std::getline(input, line);
+        std::istringstream coord(line);
+        coord >> positions[i][0] >> positions[i][1] >> positions[i][2];
+    }
 
+    //closing input
+    input.close();
 
+    //writing CONTCAR file
+    std::ofstream output("CONTCAR");
 
+    //atom names
+    for(int i=0; i<n_atom_types; i++){
+       output <<  atom_name[i] << " ";
+    }
+    output << " " << std::endl;
 
+    //charges
+    output << value << std::endl;
 
-    
+    //box dimensions
+    for(int i=0; i<3; i++){
+        output << "\t" << box_dim[i][0] << " " << box_dim[i][1] << " " << box_dim[i][2] << std::endl;
+    }
+
+    //number of atoms per type
+    for(int i=0; i<n_atom_types; i++){
+        output << n_atoms_per_type[i] << " ";
+    }
+    output << std::endl;
+
+    //coordinate system
+    output << coordinate_sys << std::endl;
+
+    //positions of atoms
+    for(int i=0; i<total_n_atoms; i++){
+        output << positions[i][0] << " " << positions[i][1] << " " << positions[i][2] << std::endl;
+    }
+
+    output.close();
+
+    return 0;
 }
