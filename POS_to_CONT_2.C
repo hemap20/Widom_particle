da13d8b29205d9547ed78c535232a121a479d3a7
hemap20 <105234527+hemap20@users.noreply.github.com>
2024-06-24 12:49:04 +0000

    pos_to_contc_2

---
+++ POS_to_CONT_2.C
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void info_in(const string& filename, vector<Atom>& atoms, vector<vector<double>>& lattice){
+
+
+}
