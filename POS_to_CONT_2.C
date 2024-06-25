8ff9c460bc3e97ca4aad97b36b2e221d24c3f1e1
hemap20 <105234527+hemap20@users.noreply.github.com>
2024-06-25 08:19:01 +0000

    pos_to_cont_2 func

--- POS_to_CONT_2.C
+++ POS_to_CONT_2.C
@@ -3,9 +3,11 @@
 #include <vector>
 #include <iomanip>
 #include <string>
+#include <chrono> 
 #include <sstream>
 
 using namespace std;
+using namespace chrono;
 
 void read_input(const string& filename, vector<string>& atom_name, int& n_atom_types, int& total_n_atoms,
                 double& value, vector<vector<double>>& box_dim, vector<int>& n_atoms_per_type,
@@ -119,6 +121,11 @@ int main(int argc, char* argv[]) {
     string input_name = argv[1];
     string output_name = argv[2];
 
+    //start time
+    auto start_time = high_resolution_clock::now();
+    auto start_time_str = system_clock::to_time_t(start_time);
+    cout << "Start time: " << put_time(localtime(&start_time_str), "%Y-%m-%d %X") << endl;
+
     // Declare variables
     vector<string> atom_name;
     int n_atom_types = 0;
@@ -135,5 +142,15 @@ int main(int argc, char* argv[]) {
     // Print CONTCAR
     print_CONTCAR(output_name, atom_name, n_atom_types, total_n_atoms, value, box_dim, n_atoms_per_type, coordinate_sys, positions);
 
+    //end time
+    auto end_time = high_resolution_clock::now();
+    auto end_time_str = system_clock::to_time_t(end_time);
+    cout << "End time: " << put_time(localtime(&end_time_str), "%Y-%m-%d %X") << endl;
+
+    //print time taken
+    duration<double> elapsed_time = duration_cast<duration<double>>(end_time - start_time);
+    cout << "Processing time: " << fixed << setprecision(6) << elapsed_time.count() << " seconds" << endl;
+
     return 0;
 }
+
